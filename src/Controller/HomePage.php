3b4b4c7dfe3c1ee97d9087b229498eed8fcddad2
gRAKLECLER <guillaume.raklecler@meero.com>
2022-01-22 10:01:41 +0100

    first commit

---
+++ src/Controller/HomePage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomePage extends AbstractController
+{
+    /**
+     * @return Response
+     * @Route("/")
+     */
+    public function homepage(): Response
+    {
+        $name = "guillaume";
+        return $this->render('base.html.twig', [
+            'name' => $name
+        ]);
+
+    }
+}
